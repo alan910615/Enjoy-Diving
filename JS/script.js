5773f2a45273e634a43c6ab18b1af50ec3268c04
Alan <show910615@gmail.com>
2021-01-03 21:17:11 +0800

    css更改

--- JS/script.js
+++ JS/script.js
@@ -12,61 +12,57 @@ hamburg.addEventListener('click', function() {
 });
 
 //購物車換圖 start
-
-let S_photo = document.querySelectorAll('.Small_photo');
-for (let i = 0; i < S_photo.length; i++) {
-	S_photo[i].addEventListener('mouseover', function(e) {
-		document.getElementById('big').src = e.target.src;
-	});
-}
-
+	let S_photo = document.querySelectorAll('.Small_photo');
+	for (let i = 0; i < S_photo.length; i++) {
+		S_photo[i].addEventListener('mouseover', function(e) {
+			document.getElementById('big').src = e.target.src;
+		});
+	}
 //購物車換圖 end
 
 // for shoping cart start
-
-//呼叫出rwd的側邊藍
-var btn_to_Checkout = document.getElementsByClassName('to_Checkout');
-var gray_block = document.querySelector('.gray_block');
-var Rwd_Checkout = document.getElementsByClassName('Rwd_Checkout')[0];
-
-
-
-
-btn_to_Checkout[0].addEventListener('click', function() {
-	Rwd_Checkout = document.getElementsByClassName('Rwd_Checkout')[0];
-	Rwd_Checkout.classList.toggle('on');
-	gray_block.classList.toggle('close1');
-});
-
-// 側邊欄出來後  點擊蒙版會收回
-gray_block.addEventListener('click', function() {
-	gray_block.classList.add('close1');
-	Rwd_Checkout.classList.add('on');
-});
-
-//點擊刪除會移除選項
-
-var remove = document.getElementsByClassName('close');
-for (let i = 0; i < remove.length; i++) {
-	remove[i].addEventListener('click', function(e) {
-		var Remove_Product = e.target.closest('tr');
-		Remove_Product.remove();
-		
-
-
+	//呼叫出rwd的側邊藍
+	var btn_to_Checkout = document.getElementsByClassName('to_Checkout');
+	var gray_block = document.querySelector('.gray_block');
+	var Rwd_Checkout = document.getElementsByClassName('Rwd_Checkout')[0];
+	btn_to_Checkout[0].addEventListener('click', function() {
+		Rwd_Checkout = document.getElementsByClassName('Rwd_Checkout')[0];
+		Rwd_Checkout.classList.toggle('on');
+		gray_block.classList.toggle('close1');
 	});
-}
 
-//	選擇數量會計算價錢
-function count() {
-	var select = document.getElementsByClassName('select');
+	// 側邊欄出來後  點擊蒙版會收回
+	gray_block.addEventListener('click', function() {
+		gray_block.classList.add('close1');
+		Rwd_Checkout.classList.add('on');
+	});
 
-	for (let i = 0; i < select.length; i++) {
-		var count = select[i].closest('tr').querySelector('.count_price');
-		count.innerHTML = select[i].value * 1000;
+	//點擊刪除會移除選項
+	var remove = document.getElementsByClassName('close');
+	for (let i = 0; i < remove.length; i++) {
+		remove[i].addEventListener('click', function(e) {
+			var Remove_Product = e.target.closest('tr');
+			Remove_Product.remove();
+			count();
+		});
 	}
-};
-count();
-//當頁面load後 再次更新價錢
 
+	//	選擇數量會計算價錢
+	function count() {
+		var select = document.getElementsByClassName('select');
+		var tr = document.getElementsByClassName('product');
+		var count_price = document.getElementsByClassName('count_price'); //商品的金額
+		var countPrice = document.getElementById('countPrice'); //結帳總覽的商品總計
+		for (let i = 0; i < select.length; i++) {
+			var count = select[i].closest('tr').querySelector('.count_price');
+			count.innerHTML = select[i].value * 1000;
+		}
+		var total = 0;
+		for(let i = 0; i < tr.length; i++){
+			total += parseInt(count_price[i].innerText);
+		}
+		countPrice.innerText = total;
+	};
+	count();
+	//當頁面load後 再次更新價錢
 // for shoping cart end
